4dcdf039d74a981183daaa6186710566908e4ca4
shivangi-khemka99 <shivangikhemka99@gmail.com>
2024-02-07 23:38:17 +0530

    Day 5 done

---
+++ day-5/question-3/code.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        next_greater = {}
+
+        for num in reversed(nums2):
+            while stack and num>stack[-1]:
+                stack.pop()
+            next_greater[num] = stack[-1] if stack else -1
+            stack.append(num)
+        result = [next_greater[num] for num in nums1]
+        return result
+        #O(N+M)
