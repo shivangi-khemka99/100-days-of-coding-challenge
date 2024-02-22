ec4cb02ed848e69d2f2ce7ff15f883ae5cb743af
shivangi-khemka99 <shivangikhemka99@gmail.com>
2024-02-22 21:03:56 +0530

    Day 20 done

---
+++ day-20/question-2/code.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        tempA, tempB = headA, headB
+
+        while tempA != tempB:
+            tempA = tempA.next if tempA else headB
+            tempB = tempB.next if tempB else headA
+        return tempA
